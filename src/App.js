46a1ef0ea6344583b3537c505af128e17e75be83
Rachel Padworski <rachelpadworski@gmail.com>
2021-01-31 19:28:18 -0700

    adds routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 // import ItemsContainer from './containers/itemsContainer';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Default from './components/Default';
+import Details from './components/Details';
+import Cart from './components/Cart';
 
 class App extends React.Component {
   render() {
     return (
-      
+        <>
+        <Navbar />
+        <Switch>
+          <Route path="/details" component={Details}></Route>
+          <Route path="/cart" component={Cart}></Route>
+          <Route exact path="/" component={ProductList}></Route>
+          <Route component={Default}></Route>
+        </Switch>
+      </>
     );
   }
 }
