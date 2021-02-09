7b32942bc4b2379a30fc937f379cdc8a274c71e3
Rachel Padworski <rachelpadworski@gmail.com>
2021-02-08 18:28:45 -0700

    addslinks and button to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../logo-thumbnail.png'
+import cart from '../shopping-cart.png'
+
 
 class Navbar extends React.Component {
     render() {
         return(
-            <div>
-                <h3>Hello from Navbar</h3>
-            </div>
-        )
+            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+                <Link to="/">
+                    <img src={ logo } alt="store" className="navbar-brand"/>
+                </Link>
+                <ul className = "navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                        <Link to="/" className="nav-link">
+                            Products
+                        </Link>
+                    </li>
+                </ul>
+                <Link to='/cart' className="ml-auto">
+                    <button>
+                        <img src={ cart } alt="cart" className="fas fa-cart-plus" />
+                        My Cart
+                    </button>
+                </Link>
+            </nav>
+        );
     }
 };
 
