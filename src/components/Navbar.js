94cda602da9f40fa4c4f87d053abfc0c768e96f2
Rachel Padworski <rachelpadworski@gmail.com>
2021-02-08 18:48:00 -0700

    adds styled components to cart button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../logo-thumbnail.png'
-import cart from '../shopping-cart.png'
-
+import logo from '../logo-thumbnail.png';
+import cart from '../shopping-cart.png';
+import styled from 'styled-components';
 
 class Navbar extends React.Component {
     render() {
@@ -19,14 +19,37 @@ class Navbar extends React.Component {
                     </li>
                 </ul>
                 <Link to='/cart' className="ml-auto">
-                    <button>
-                        <img src={ cart } alt="cart" className="fas fa-cart-plus" />
+                    <ButtonContainer>
+                        <span className="mr-2">
+                            <img src={ cart } alt="cart" className="fas fa-cart-plus" />
+                        </span>
                         My Cart
-                    </button>
+                    </ButtonContainer>
                 </Link>
             </nav>
         );
     }
 };
+
+const ButtonContainer = styled.button`
+    text-transform: capitalize;
+    font-size: 1.4rem;
+    background: transparent;
+    border: 0.05rem solid var(--lightBlue);
+    color: var(--lightBlue);
+    border-radius: 0.5rem;
+    padding: 0.2rem 0.5rem;
+    cursor: pointer;
+    margin: 0.2rem 0.5rem 0.2rem 0;
+    transition: all 0.5s ease-in-out;
+    &:hover{
+        background: var(--lightBlue);
+        color: var(--mainBlue);
+    }
+    &:focus{
+        outline: none;
+    }
+`;
+
 
 export default Navbar;
