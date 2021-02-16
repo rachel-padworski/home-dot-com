8e624db2ebcfd7febda311521cd37bd34035d340
Rachel Padworski <rachelpadworski@gmail.com>
2021-02-15 19:21:15 -0700

    adds setProducts and lifecycle method

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { ProductConsumer } from '../context';
+import PropTypes from 'prop-types';
 
 export default class Product extends Component {
     render() {
@@ -39,6 +40,17 @@ export default class Product extends Component {
             </ProductWrapper>
         )
     }
+}
+
+// propTypes allows us to check the props to make sure we have the correct prop type. 
+Product.propTypes = {
+    product: PropTypes.shape({
+        id: PropTypes.number,
+        img: PropTypes.string,
+        title: PropTypes.string,
+        price: PropTypes.number,
+        inCart: PropTypes.bool
+    }).isRequired
 }
 
 const ProductWrapper = styled.div`
--- src/context.js
+++ src/context.js
@@ -7,8 +7,24 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
     state = {
-        products: storeProducts,
+        products: [],
         detailProduct: detailProduct
+    }
+    // gets the new set of values and not a copy of the values.
+    componentDidMount() {
+        this.setProducts();
+    }
+
+    setProducts = () => {
+        let tempProducts = [];
+        storeProducts.forEach(item => {
+            const singleItem = {...item};
+            tempProducts = [...tempProducts, singleItem];
+
+        })
+        this.setState(() => {
+            return { products: tempProducts };
+        })
     }
 
     handleDetail = () => {
